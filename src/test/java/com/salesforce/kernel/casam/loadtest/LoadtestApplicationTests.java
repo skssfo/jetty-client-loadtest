eba8c7d3a83dac37a11c75038190c36ab4cdde5c
Sathish <sathish.santhanam@salesforce.com>
2017-12-12 09:25:37 -0800

    initial commit

---
+++ src/test/java/com/salesforce/kernel/casam/loadtest/LoadtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.salesforce.kernel.casam.loadtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoadtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
